6b1330fd605b18776ac10fb332cec24ff175e68b
Joseph Spencer <104227087+Jarseph@users.noreply.github.com>
2022-12-30 21:33:29 -0700

    Create palindrome.js

---
+++ palindrome.js
@@ -0,0 +1,26 @@
+/* 
+Remove all spaces and punctuation from the sentence and make all characters lowercase.
+Declare a variable middle and initialize it to half the length of the sentence, rounding down to an integer value for odd-length strings. For example, if the sentence is of length 7, then middle is 3.
+Initialize a new stack.
+Iterate through the sentence, from the first character up to middle. Push each character onto the stack.
+Iterate from middle to the end of the sentence. If the sentence is an odd length, then iterate from middle+1 to skip the middle character of the sentence. On each iteration, pop a character from the stack and compare it to the current character. If they don't match, return false.
+When the loop is done, return true.
+*/
+
+function palindrome(sentence) {
+  sentence = sentence.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+  let middle = Math.floor(sentence.length / 2);
+  const stack = new Stack();
+  for (let i = o; i < middle; i++) {
+    stack.push(sentence[i]);
+  }
+  middle += sentence.length % 2 === 0 ? 0 : 1;
+  for (let i = middle, limit = sentence.length; i < limit; i++) {
+    if (sentence[index] !== stack.pop()) {
+      return false;
+    }
+  }
+  return true;
+};
+
+module.exports = palindrome;
